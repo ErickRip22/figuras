11ae84d467021bbe240b210ac3b9a3407f9eb954
Erick Raúl Ibarra Pérez <erick_rp31@hotmail.com>
2017-10-04 20:59:03 -0500

    Corrección de líneas
    Se quitaron espacios en blanco y se agregaron ignores para flake8 en
    líneas no adaptables.

--- bdd/tests/features/FigurasTest.py
+++ bdd/tests/features/FigurasTest.py
@@ -1,6 +1,6 @@
 '''
 Jared
-Erick Raul Ibarra Perez 
+Erick Raul Ibarra Perez
 '''
 import unittest
 from fig import Figuras
@@ -9,8 +9,7 @@ from fig import Figuras
 class FiguraTest (unittest.TestCase):
 
     def setUp(self):
-        self.fig=Figuras()
-
+        self.fig = Figuras()
 
     def test_calcular_circulo(self):
         self.fig.calcular_area_circulo(10)
@@ -21,13 +20,13 @@ class FiguraTest (unittest.TestCase):
         self.assertEqual(25, self.fig.get_area())
 
     def test_rectangulo(self):
-        self.fig.calcular_area_rectangulo(10,5)
+        self.fig.calcular_area_rectangulo(10, 5)
         self.assertEqual(50, self.fig.get_area())
 
     def test_trapecio(self):
         self.fig.calcular_area_trapecio(3, 4, 5.0)
         self.assertEqual(13.5, self.fig.get_area())
+
+
 if __name__ == '__main__':
     unittest.main()
-
-
--- bdd/tests/features/steps.py
+++ bdd/tests/features/steps.py
@@ -2,31 +2,38 @@
 from lettuce import step, world
 from fig import Figuras
 
+
 @step(u'cuando realizo la operacion')
 def cuando_realizo_la_operacion(step):
     pass
+
 
 @step(u'Dado que ingreso el lado "([^"]*)"')
 def dado_que_ingreso_el_lado_group1(step, lado):
     world.figs = Figuras()
     world.figs.calcular_area_cuadrado(int(lado))
 
+
 @step(u'entonces obtengo el area "([^"]*)"')
 def entonces_obtengo_el_area_group1(step, esperado):
-    obtenido =  world.figs.get_area()
-    assert float(esperado) == obtenido, 'El resultado esperado es' + esperado 
-        + ' y el obtenido es ' + obtenido
+    obtenido = world.figs.get_area()
+    assert float(esperado) == obtenido, 'El resultado esperado es' + esperado + ' y el obtenido es ' + obtenido  # noqa: E501
+
 
 @step(u'Dado que ingreso los lados "([^"]*)" por "([^"]*)"')
 def dado_que_ingreso_los_lados_group1_por_group2(step, num1, num2):
     world.figs = Figuras()
     world.figs.calcular_area_rectangulo(int(num1), int(num2) + 0.0)
+
+
 @step(u'Dado que ingreso el radio "([^"]*)"')
 def dado_que_ingreso_el_radio_group1(step, radio):
-	world.figs = Figuras()
-	world.figs.calcular_area_circulo(int(radio))
-@step(u'Dado que ingreso la base menor "([^"]*)", base mayor "([^"]*)" y altura "([^"]*)"') # noqa: E731
-def dado_que_ingreso_la_base_menor_group1_base_mayor_group2_y_altura_group3(step, 
-        basema, baseme, altura):
-	world.figs = Figuras()
-	world.figs.calcular_area_trapecio(float(altura), float(basema), float(baseme))
+    world.figs = Figuras()
+    world.figs.calcular_area_circulo(int(radio))
+
+
+@step(u'Dado que ingreso la base menor "([^"]*)", base mayor "([^"]*)" y altura "([^"]*)"')  # noqa: E501
+def dado_que_ingreso_la_base_menor_group1_base_mayor_group2_y_altura_group3(step, basema, baseme, altura):  # noqa: E501
+    world.figs = Figuras()
+    world.figs.calcular_area_trapecio(
+        float(altura), float(basema), float(baseme))
